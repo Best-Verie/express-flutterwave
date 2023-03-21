858d53693a63954451424bb090f4300121f563d0
Best-Verie <irabestverie2016@gmail.com>
2023-03-21 14:54:49 +0200

    fx: second approach to passing transaction

--- index.js
+++ index.js
@@ -1,45 +1,78 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const bodyParser = require('body-parser');
-const Flutterwave = require('flutterwave-node');
 
-dotenv.config();
-
+const express = require("express");
+const Flutterwave = require("flutterwave-node");
+const dotenv = require('dotenv')
 const app = express();
+app.use(express.json());
+dotenv.config()
+const flutterwave = new Flutterwave(process.env.FLUTTERWAVE_PUBLIC_KEY, process.env.FLUTTERWAVE_SECRET_KEY, process.env.FLUTTERWAVE_ENCRYPTION_KEY);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const flutterwave = new Flutterwave(process.env.FLUTTERWAVE_PUBLIC_KEY, process.env.FLUTTERWAVE_SECRET_KEY);
-
-app.post('/invoices', async (req, res) => {
-    const { customer, amount, currency, description } = req.body;
-
-    const data = {
-        tx_ref: `INV-${Math.floor(Math.random() * 1000000)}`,
-        amount,
-        currency,
-        payment_options: 'card',
-        redirect_url: 'https://example.com/redirect',
-        customer: {
-            email: customer.email,
-            phone_number: customer.phone_number,
-            name: customer.name,
-        },
-        customizations: {
-            title: 'My Store',
-            description,
-            logo: 'https://example.com/logo.png',
-        },
-    };
-
+app.post("/card-charge", async (req, res) => {
     try {
-        const response = await flutterwave.flutterwaveInvoices(data);
-        res.json({ url: response.data.data.link });
+        // Get the card details from the request body
+        // const { card_number, cvv, expiry_month, expiry_year, pin } = req.body;
+
+        console.log(req.body)
+
+        // Set up the Flutterwave payment details
+        // const paymentData = {
+        //     txRef: "test_transaction",
+        //     amount: "100",
+        //     currency: "NGN",
+        //     payment_options: "card",
+        //     card: {
+        //         number: card_number,
+        //         cvv: cvv,
+        //         expiry_month: expiry_month,
+        //         expiry_year: expiry_year,
+        //         pin: pin
+        //     },
+        //     customer: {
+        //         email: "user@example.com",
+        //         phonenumber: "08123456789",
+        //         name: "John Doe"
+        //     },
+        //     meta: {
+        //         consumer_id: 23,
+        //         consumer_mac: "92a3-912ba-1192a"
+        //     },
+        //     on_success: function (response) {
+        //         console.log(response);
+        //         res.send("Payment successful!");
+        //     },
+        //     on_error: function (error) {
+        //         console.log(error);
+        //         res.send("Payment failed.");
+        //     },
+        //     on_close: function () {
+        //         console.log("Payment closed.");
+        //     }
+        // };
+        // console.log("paymentData", paymentData)
+
+        const details = {
+            "cardno": "5531886652142950",
+            "cvv": "564",
+            "expirymonth": "09",
+            "expiryyear": "32",
+            "currency": "NGN",
+            "amount": "100",
+            "fullname": "Yolande Aglaé Colbert",
+            "email": "user@example.com",
+            "txRef": "MC-3243e",
+            "redirect_url": "https://www.flutterwave.ng"
+        };
+
+        // Make the Flutterwave API call to charge the card
+        const response = await flutterwave.Card.charge(details);
+        res.json(response);
     } catch (error) {
-        res.json({ error: error.message });
+        console.log(error);
+        res.status(500).send("An error occurred.");
     }
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 
